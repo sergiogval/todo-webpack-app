aa2a3801a3a0dd16f84e8c7be17c0b05a058be57
sergiogval <sergioval@gmail.com>
2022-01-19 14:33:16 -0600

    Add dist folder files

--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,8 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
   },
 };
