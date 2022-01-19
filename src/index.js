d5f785ad9db1594cf4bc61f5494f0dac489ce9a9
sergiogval <sergioval@gmail.com>
2022-01-19 13:28:25 -0600

    Add webpack and neccesary dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import _ from 'lodash';
 
+export default function component() {
+  const element = document.createElement('div');
 
- function component() {
-   const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-   return element;
- }
-
- document.body.appendChild(component());
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
+}
+document.body.appendChild(component());
