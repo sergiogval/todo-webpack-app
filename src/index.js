80708d33791aa79c2c503c726be77e61e3c31f7a
sergiogval <sergioval@gmail.com>
2022-01-26 17:44:41 -0600

    Add src files and dependencies.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import _ from 'lodash';
+const [first, second, third] = [
+  {id: 1, description: 'tarea 1', completed: false},
+  {id: 2, description: 'tarea 2', completed: false},
+  {id: 3, description: 'tarea 3', completed: false},
+]
 
-export default function component() {
-  const element = document.createElement('div');
+let arr = [first, second, third]
+let todos = document.getElementById('todos')
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
+for(let i = 0; i <= arr.length, arr += 1;) {
+  todos = document.createElement(`li`)
+  todos.textContent = `
+  <li class="todoItem">
+  <div>
+  <input class="checker" id=${ arr[i].id } type="checkbox" value="${ arr[i].completed }"/>
+  <label for="${ arr[i].id }">${ arr[i].description }</label>
+  </div>
+  <button type="button" class="positioner">
+  <i class="fa fa-ellipsis-v"></i>
+  </button>
+  </li>`
 }
-document.body.appendChild(component());
+document.appendChild(todos)
+
